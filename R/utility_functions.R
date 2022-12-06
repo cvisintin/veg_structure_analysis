84f0e72c935800b983e0e4e5d487ca7052d73752
Casey Visintin <casey.visintin@unimelb.edu.au>
2022-12-06 11:54:59 +1100

    added feature to consider varying base reference heights of individual plants

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -8,12 +8,12 @@ process_structure <- function(heights = c(0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0), #
                               grid # Grid of polygons to be used in the analysis
 ) {
   
-  # Calculate all plant heights and widths for the given year
+  # Calculate all relative plant heights and widths for the given year (from reference height)
   plant_heights <- locations$max_height / locations$year_max * pmin(year, locations$year_max)
   plant_widths <- locations$max_width / locations$year_max * pmin(year, locations$year_max)
   
   # Generate id lists for plants that intersect each height datum for the given year
-  plant_idx <- lapply(heights, function(height) which(plant_heights >= height))
+  plant_idx <- lapply(heights, function(height) which(plant_heights + locations$ref_height >= height | height >= locations$ref_height))
   
   proportion_grids <- lapply(heights, function(height) {
     
@@ -29,10 +29,10 @@ process_structure <- function(heights = c(0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0), #
       
       # Generate multipliers for width based on assumed geometry (note, this is moderated by height and type)
       mults <- sapply(ids, function(id) {
-        ifelse(locations$type[id] == 'tree' & height < plant_heights[id] / 2, 0,
-               ifelse(locations$type[id] == 'tree' & height >= plant_heights[id] / 2, sqrt(pmax(0, (plant_heights[id] * 0.25)^2 - (height - (plant_heights[id] * 0.75))^2)) / (plant_heights[id] * 0.25),
-                      ifelse(locations$type[id] == 'shrub', sqrt(pmax(0, (plant_heights[id] * 0.5)^2 - (height - (plant_heights[id] * 0.5))^2)) / (plant_heights[id] * 0.5),
-                             height / plant_heights[id])))
+        ifelse(locations$type[id] == 'tree' & height < plant_heights[id]  / 2, 0,
+               ifelse(locations$type[id] == 'tree' & height >= plant_heights[id] / 2, sqrt(pmax(0, (plant_heights[id] * 0.25)^2 - ((height - locations$ref_height[id]) - (plant_heights[id] * 0.75))^2)) / (plant_heights[id] * 0.25),
+                      ifelse(locations$type[id] == 'shrub', sqrt(pmax(0, (plant_heights[id] * 0.5)^2 - ((height - locations$ref_height[id]) - (plant_heights[id] * 0.5))^2)) / (plant_heights[id] * 0.5),
+                             (height - locations$ref_height[id]) / plant_heights[id])))
       })
       
       # Create a buffered region around all plants based on geometry type for the given year
