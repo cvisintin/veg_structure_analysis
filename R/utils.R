d696b658803b9aab7003c522c30290a6d1613ca2
Casey Visintin <casey.visintin@unimelb.edu.au>
2023-01-13 19:52:09 +1100

    restructuring files and functions

--- R/utils.R
+++ R/utils.R
@@ -164,7 +164,348 @@ extract_1m_coverage <- function(spatial_list) {
   })
 }
 
+convert_combine <- function(point_locations, # Spatial geometry and attributes of plants
+                            polygon_locations # Spatial geometry and attributes of planted areas
+) {
+  n_polys <- nrow(polygon_locations)
+  
+  point_list <- lapply(seq_len(n_polys), function(i) {
+    data <- st_drop_geometry(polygon_locations[i, ])
+    cell_size <- 1 / sqrt(polygon_locations$spacing[i])
+    grid <- st_make_grid(st_bbox(polygon_locations$geometry[i]), cellsize = cell_size, what = "centers")
+    points <- st_intersection(grid, polygon_locations$geometry[i])
+    n_points <- length(points)
+    points <- points[sample(seq_len(n_points), floor(n_points * polygon_locations$coverage[i] / 100))]
+    st_sf(data, geometry = points)
+  })
+  
+  new_plant_points <- do.call(rbind, point_list)
+  
+  rbind(point_locations, new_plant_points)
+}
 
+process_structure <- function(point_locations, # Spatial geometry and attributes of plants
+                              obstructions, # Geometry that may obstruct plant growth in horizontal plane
+                              boundary, # Geometry that defines the boundary of the site
+                              cut_heights = c(0.3, 1.0, 1.7, 2.4, 3.1), # Height datums in meters
+                              years = 1, # Years of interest
+                              hex_gridshape = TRUE, # Use hexagonal-shaped grid cells in the analysis; default is yes
+                              cellarea, # Target cell area in square meters
+                              veg_layers_path = NULL, # Path to folder where vegetation layers should be written
+                              overlap_grids_path = NULL # Path to folder where grid layers should be written
+) {
+  
+  # Create grid from boundary based on specified grid shape and approximate cell size
+  master_grid <- create_grid(boundary = boundary, hex_gridshape = hex_gridshape, cellarea = cellarea)
+  
+  # # Get grid spacing
+  # grid_spacing <- attr(master_grid, 'gridspacing')
+  
+  # Determine cell spacing distance
+  cell_dist <- sqrt(2 * cellarea / (3 * sqrt(3))) * sqrt(3)
+  if(hex_gridshape == FALSE) cell_dist <- sqrt(cellarea)
+  
+  # # Determine maximum height of vegetation plus 5%
+  # max_veg_height <- max(point_locations$max_height) * 1.05
+  # 
+  # # Number of vertical layers at cell spacing distance within maximum vegetation height
+  # n_vert_layers <- max_veg_height %/% cell_dist
+  # 
+  # # If cut_heights are not specified, use regular spacing based on distance between cells
+  # if(is.null(cut_heights)) cut_heights <- (1:n_vert_layers) * cell_dist
+  # 
+  # # Set default switch for added 1m cut height
+  # add_1m <- FALSE
+  # 
+  # # If 1m is not specified in cut heights, add it
+  # if(!1 %in% cut_heights) {
+  #   add_1m <- TRUE
+  #   cut_heights <- sort(c(1, cut_heights))
+  # }
+  
+  out <- lapply(years, function(year) {  
+    
+    print(paste0("Processing year ", year))
+    
+    # Calculate all relative plant heights and widths for the given year (from reference height)
+    plant_heights <- point_locations$max_height * (pmin(year, point_locations$year_max) / point_locations$year_max)
+    plant_widths <- point_locations$max_width * (pmin(year, point_locations$year_max) / point_locations$year_max)
+    
+    # Generate id lists for plants that intersect each height datum for the given year
+    plant_idx <- lapply(cut_heights, function(cut_height) which(plant_heights + point_locations$ref_height >= cut_height & cut_height >= point_locations$ref_height))
+    
+    proportion_grids <- lapply(cut_heights, function(cut_height) {
+      
+      # Copy master grid
+      grid <- master_grid
+      
+      # Add new attribute to grid
+      grid$prop_ol <- as.numeric(NA)
+      
+      # Extract ids for vegetation that are within the height category
+      ids <- plant_idx[[which(cut_heights == cut_height)]]
+      
+      # Verify if any vegetation exists within height category 
+      if(length(ids) > 0) {
+        
+        # Generate multipliers for width based on one of seven specified geometry forms (note, this is moderated by plant height)
+        mults <- sapply(ids, function(id) {
+          adj_cut_height <- cut_height - point_locations$ref_height[id]
+          plant_height <- plant_heights[id]
+          do.call(point_locations$form[id], list(adj_cut_height = adj_cut_height, plant_height = plant_height))
+        })
+        
+        # Create a buffered region around all plants based on geometry forms for the given year
+        buffered_area <- st_union(st_buffer(point_locations[ids, ], (plant_widths[ids] / 2) * mults))
+        
+        # Determine which obstructions are above cut height
+        obstruction_idx <- which(obstructions$height > cut_height)
+        
+        # Remove areas with obstructions (e.g. buildings)
+        planted_area <- st_difference(buffered_area, obstructions$geometry[obstruction_idx])
+        
+        # If path is provided, write out vegetation footprints for given year and selected heights
+        # using centimeters to designate height (padded with zeros)
+        if(!is.null(veg_layers_path)) {
+          st_write(planted_area, paste0(veg_layers_path,
+                                        "year_",
+                                        sprintf("%02d", year),
+                                        "_height_",
+                                        sprintf("%04d", ceiling(cut_height * 100)),
+                                        "_veg.shp"), append = FALSE)
+        }
+        
+        # Identify the grid cells that have overlap with vegetation
+        overlap_idx <- st_intersects(grid$geometry, st_union(planted_area), sparse = FALSE)
+        
+        # Determine the area of overlap in each qualified grid cell
+        areas <- st_area(st_intersection(grid$geometry[overlap_idx], st_union(planted_area)))
+        
+        # Record proportions of overlap to eligible grids
+        grid$prop_ol[overlap_idx] <- areas / st_area(grid$geometry[1])
+        
+      }
+      
+      # If path is provided, write out grids for given year and selected heights using centimeters
+      # to designate height (padded with zeros)
+      if(!is.null(overlap_grids_path)) {
+        st_write(grid, paste0(overlap_grids_path,
+                              "year_",
+                              sprintf("%02d", year),
+                              "_height_",
+                              sprintf("%04d", ceiling(cut_height * 100)),
+                              "_grd_",
+                              ifelse(hex_gridshape, "hex", "rect") ,
+                              ".shp"), append = FALSE)
+      }
+      
+      grid
+      
+    })
+    
+    names(proportion_grids) <- paste0("cut_", sprintf("%04d", ceiling(cut_heights * 100)))
+    
+    proportion_grids
+    
+  })
+  
+  names(out) <- paste0("year_", sprintf("%02d", years))
+  
+  # # Add attribute to identify added cut
+  # attr(out, 'cut_1m') <- FALSE
+  # if(add_1m) attr(out, 'cut_1m') <- TRUE
+  
+  # Add attribute to identify gridshape
+  attr(out, 'gridshape') <- "hex"
+  if(hex_gridshape == FALSE) attr(out, 'gridshape') <- "rect"
+  
+  # Add attribute to identify grid spacing
+  attr(out, 'gridspacing') <- cell_dist
+  
+  out
+}
+
+estimate_connectivity <- function(spatial_list,
+                                  threshold = 0.3) {
+  # Extract unique years from file list
+  years <- names(spatial_list)
+  n_year_groups <- length(years)
+  
+  # Extract unique heights from file list and convert to numeric and meter units
+  heights <- names(spatial_list[[1]])
+  heights <- as.numeric(substr(heights, (nchar(heights) + 1) - 4, nchar(heights))) / 100
+  n_heights <- length(heights)
+  
+  master_grid <- spatial_list[[1]][[1]]
+  
+  grid_spacing <- attr(spatial_list, 'gridspacing')
+  
+  search_dist <- grid_spacing * 1.5
+  
+  buffers <- st_buffer(st_centroid(master_grid$geometry), search_dist)
+  
+  points <- st_centroid(master_grid$geometry)
+  points <- st_sf(data.frame("id" = 1:length(points)), geometry = points)
+  
+  idx <- master_grid$id
+  
+  # Create master neighborhood list
+  nb_list <- lapply(idx, function(p) {
+    ids <- points$id[st_intersects(points, buffers[p], sparse = FALSE, prepared = FALSE)]
+    ids <- setdiff(ids, p)
+  })
+  
+  scores_df <- data.frame(matrix(NA, nrow = n_year_groups, ncol = n_heights))
+  colnames(scores_df) <- names(spatial_list[[1]])
+  
+  for (i in seq_len(n_year_groups)) {
+    
+    spatial_year <- spatial_list[[i]]
+    
+    scores <- sapply(seq_len(n_heights), function(j) {
+      
+      grid <- spatial_year[[j]]
+      
+      grid$prop_ol[is.na(grid$prop_ol)] <- 0
+      grid$prop_ol <- ifelse(grid$prop_ol > threshold, 1, 0)
+      
+      mean(sapply(idx, function(p) {
+        sum(grid$prop_ol[nb_list[[p]]]) / length(sum(grid$prop_ol[nb_list[[p]]]))
+      }))
+      
+    })
+    
+    scores_df[i, ] <- scores
+    
+  }
+  
+  scores_df
+  
+}
+
+create_images <- function(spatial_list, path) {
+  
+  # Extract unique years from file list
+  years <- names(spatial_list)
+  n_year_groups <- length(years)
+  
+  # Extract unique heights from file list and convert to numeric and meter units
+  cut_heights <- names(spatial_list[[1]])
+  cut_heights <- as.numeric(substr(cut_heights, (nchar(cut_heights) + 1) - 4, nchar(cut_heights))) / 100
+  
+  sapply(seq_len(n_year_groups), function(j) {
+    
+    spatial_year <- spatial_list[[j]]
+    
+    plot_list <- list()
+    
+    for (i in 1:length(spatial_year)) {
+      
+      # Load processed spatial data
+      spatial_data <- spatial_year[[i]]
+      
+      # Rotate spatial data
+      spatial_data <- rotate_data(spatial_data)
+      
+      # Set annotation position
+      x <- st_bbox(spatial_data)[3] * 0.999995
+      y <- st_bbox(spatial_data)[2] * 1.000001 
+      
+      # Create plot of proportional overlap grid at currently specified height
+      plot_list[[i]] <- ggplot() +
+        geom_sf(data = spatial_data, aes(fill = prop_ol), color = 'gray40', size = 0.05, show.legend = FALSE) +
+        annotate("text", label = paste0(cut_heights[i], " m"), x = x , y = y, hjust = 0, color = 'gray40', size = 2) +
+        scale_fill_viridis_c(option = 'E', na.value = "transparent") +
+        theme_void()
+    } 
+    
+    # Write out PNG of overlap grids for all heights
+    png(paste0(path, "veg_structure_", years[j], ".png"), height = 150 * length(spatial_year), width = 400, pointsize = 12, res = 300)
+    grid.arrange(grobs = rev(plot_list), ncol = 1)
+    dev.off()
+    
+    NULL # Because function is not meant to return anything
+  })
+}
+
+plot_classes <- function(spatial_points, variable_name, colour_palette, image_path) {
+  st_geometry(spatial_points) <- NULL
+  spatial_points <- as.data.frame(spatial_points)
+  
+  height_ranges <- rbind(cbind(0, 0.75),
+                         cbind(0.75, 1.5),
+                         cbind(1.5, 3),
+                         cbind(3, Inf))
+  
+  ifelse(variable_name == "size",
+         data <- data.frame(
+           category = c("0-0.75m", "0.75-1.5m", "1.5-3m", "+3m"),
+           values = apply(height_ranges, 1, function(hts) length(which(spatial_points$max_height > hts[1] & spatial_points$max_height <= hts[2])))
+         ),
+         data <- data.frame(
+           category = toupper(unique(spatial_points[ , variable_name])),
+           values = sapply(unique(spatial_points[ , variable_name]), function(var) length(which(spatial_points[ , variable_name] == var)))
+         ))
+  
+  # Compute percentages
+  data$fraction = data$values / sum(data$values)
+  
+  # Compute the cumulative percentages (top of each rectangle)
+  data$ymax = cumsum(data$fraction)
+  
+  # Compute the bottom of each rectangle
+  data$ymin = c(0, head(data$ymax, n = -1))
+  
+  # Compute label position
+  data$labelPosition <- (data$ymax + data$ymin) / 2
+  
+  # Make the plot
+  ggplot(data, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = category)) +
+    geom_rect() +
+    geom_text(x = 3.5, aes(y = labelPosition, label = category), size = 4) +
+    scale_fill_manual(values = colour_palette) +
+    coord_polar(theta = "y") +
+    xlim(c(0.8, 4)) +
+    theme_void() +
+    theme(legend.position = "none") +
+    
+    geom_image(data = data.frame(xx = 0.8, yy = 0, image = image_path), mapping = aes(xx, yy, image = image), size = .3, inherit.aes = FALSE)
+}
+
+plot_percent <- function(spatial_points, variable_name, colour, label) {
+  st_geometry(spatial_points) <- NULL
+  spatial_points <- as.data.frame(spatial_points)
+  
+  props <- table(spatial_points[ , variable_name])
+  native_prop <- base::round((props[2] / sum(props)) * 100)
+  exotic_prop <- base::round((props[1] / sum(props)) * 100)
+  
+  data <- data.frame(
+    category = 1:100,
+    values = c(rep(1, native_prop), rep(0, exotic_prop))
+  )
+  
+  # Compute the cumulative percentages (top of each rectangle)
+  data$ymax = 1:100
+  
+  # Compute the bottom of each rectangle
+  data$ymin = c(0, head(data$ymax, n = -1))
+  
+  pr_palette <- c(rep(colour, native_prop),  rep("#e8e8e8", exotic_prop))
+  
+  # Make the plot
+  ggplot(data, aes(ymax = ymax, ymin = ymin, xmax = 5, xmin = 1, fill = factor(category))) +
+    geom_rect(color = "white", size = 1) +
+    geom_point(aes(x = 0, y = -max(values) * 0.5), color = "white", size = 50) +
+    #geom_text(x = 3.5, aes(y = labelPosition, label = toupper(category)), size = 4) +
+    scale_fill_manual(values = pr_palette) +
+    coord_polar(theta = "y") +
+    xlim(c(0, 5)) +
+    theme_void() +
+    theme(legend.position = "none") +
+    
+    geom_text(data = data.frame(xx = 0, yy = -max(data$values) * 0.5, label = paste0(native_prop, "%\n", label)), mapping = aes(xx, yy, label = label), size = 4, inherit.aes = FALSE)
+}
 
 
 #### Original function by Stefan Jünger
--- R/visualise.R
+++ R/visualise.R
@@ -7,267 +7,62 @@ library(png)
 library(dplyr)
 library(tidyr)
 library(viridis)
-source('R/utility.R')
+source('R/utils.R')
+
+# Load background spatial data
+plant_points <- read_sf("data/gis/Plant_Data.shp")
+planting_polygons <- read_sf("data/gis/Planting_Data.shp")
+building <- read_sf("data/gis/Building_Mass.shp")
+site_boundary <- read_sf("data/gis/Site_Boundary.shp")
+
+# Convert planting polygons to points and combine with existing plant points
+plant_points <- convert_combine(point_locations = plant_points,
+                                polygon_locations = planting_polygons)
 
 # Load grids file
 load(file = "output/years_1_30")
 
-# Extract unique years from file list
-years <- names(years_1_30)
-n_year_groups <- length(years)
-
-# Extract unique heights from file list and convert to numeric and meter units
-cut_heights <- names(years_1_30[[1]])
-cut_heights <- as.numeric(substr(cut_heights, (nchar(cut_heights) + 1) - 4, nchar(cut_heights))) / 100
-
 # Create spatial outputs
-sapply(seq_len(n_year_groups), function(j) {
-  
-  spatial_year <- years_1_30[[j]]
-  
-  plot_list <- list()
-  
-  for (i in 1:length(spatial_year)) {
-    
-    # Load processed spatial data
-    spatial_data <- spatial_year[[i]]
-    
-    # Rotate spatial data
-    spatial_data <- rotate_data(spatial_data)
-    
-    # Set annotation position
-    x <- st_bbox(spatial_data)[3] * 0.999995
-    y <- st_bbox(spatial_data)[2] * 1.000001 
-    
-    # Create plot of proportional overlap grid at currently specified height
-    plot_list[[i]] <- ggplot() +
-      geom_sf(data = spatial_data, aes(fill = prop_ol), color = 'gray40', size = 0.05, show.legend = FALSE) +
-      annotate("text", label = paste0(cut_heights[i], " m"), x = x , y = y, hjust = 0, color = 'gray40', size = 2) +
-      scale_fill_viridis_c(option = 'E', na.value = "transparent") +
-      theme_void()
-  } 
-  
-  # Write out PNG of overlap grids for all heights
-  png(paste0("figs/veg_structure_", years[j], ".png"), height = 150 * length(spatial_year), width = 400, pointsize = 12, res = 300)
-  grid.arrange(grobs = rev(plot_list), ncol = 1)
-  dev.off()
-  
-  NULL # Because function is not meant to return anything
-})
+create_images(years_1_30, path = "figs/")
 
 # Animate spatial outputs
 system('convert -delay 50 -dispose previous -loop 0 figs/* figs/animations/veg_growth.gif')
 
-
 ### Density ###
-density_data <- data.frame(
-  category = unique(plant_points$density),
-  values = sapply(unique(plant_points$density), function(dens) length(which(plant_points$density == dens)))
-)
-
-# Compute percentages
-density_data$fraction = density_data$values / sum(density_data$values)
-
-# Compute the cumulative percentages (top of each rectangle)
-density_data$ymax = cumsum(density_data$fraction)
-
-# Compute the bottom of each rectangle
-density_data$ymin = c(0, head(density_data$ymax, n = -1))
-
-# Compute label position
-density_data$labelPosition <- (density_data$ymax + density_data$ymin) / 2
-
-# Compute a good label
-density_data$label <- paste0(density_data$category, "\n value: ", density_data$values)
-
-
-gr_palette <- c("#397d53", "#b7e4c8", "#62a67c")
-
-# Make the plot
-density <- ggplot(density_data, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = category)) +
-  geom_rect() +
-  geom_text(x = 3.5, aes(y = labelPosition, label = toupper(category)), size = 4) +
-  scale_fill_manual(values = gr_palette) +
-  coord_polar(theta = "y") +
-  xlim(c(0.8, 4)) +
-  theme_void() +
-  theme(legend.position = "none") +
-  
-  geom_image(data = data.frame(xx = 0.8, yy = 0, image = "data/images/leaves_icon.png"), mapping = aes(xx, yy, image = image), size = .3, inherit.aes = FALSE)
-
-density
-
+density_plot <- plot_classes(plant_points,
+                             variable_name = "density",
+                             colour_palette = c("#397d53", "#b7e4c8", "#62a67c"),
+                             image_path = "data/images/leaves_icon.png")
 
 ### Texture ###
-texture_data <- data.frame(
-  category = unique(plant_points$texture),
-  values = sapply(unique(plant_points$texture), function(tex) length(which(plant_points$texture == tex)))
-)
-
-# Compute percentages
-texture_data$fraction = texture_data$values / sum(texture_data$values)
-
-# Compute the cumulative percentages (top of each rectangle)
-texture_data$ymax = cumsum(texture_data$fraction)
-
-# Compute the bottom of each rectangle
-texture_data$ymin = c(0, head(texture_data$ymax, n = -1))
-
-# Compute label position
-texture_data$labelPosition <- (texture_data$ymax + texture_data$ymin) / 2
-
-# Compute a good label
-texture_data$label <- paste0(texture_data$category, "\n value: ", texture_data$values)
-
-
-bl_palette <- c("#4b6c90", "#afc6e0", "#6d8eb3")
-
-# Make the plot
-texture <- ggplot(texture_data, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = category)) +
-  geom_rect() +
-  geom_text(x = 3.5, aes(y = labelPosition, label = toupper(category)), size = 4) +
-  scale_fill_manual(values = bl_palette) +
-  coord_polar(theta = "y") +
-  xlim(c(0.8, 4)) +
-  theme_void() +
-  theme(legend.position = "none") +
-  
-  geom_image(data = data.frame(xx = 0.8, yy = 0, image = "data/images/texture_icon.png"), mapping = aes(xx, yy, image = image), size = .4, inherit.aes = FALSE)
-
-texture
-
+texture_plot <- plot_classes(plant_points,
+                             variable_name = "texture",
+                             colour_palette = c("#4b6c90", "#afc6e0", "#6d8eb3"),
+                             image_path = "data/images/texture_icon.png")
 
 ### Size ###
-
-height_ranges <- rbind(cbind(0, 0.75),
-                       cbind(0.75, 1.5),
-                       cbind(1.5, 3),
-                       cbind(3, Inf))
-
-sizes_data <- data.frame(
-  category = c("0-0.75m", "0.75-1.5m", "1.5-3m", "+3m"),
-  values = apply(height_ranges, 1, function(hts) length(which(plant_points$max_height > hts[1] & plant_points$max_height <= hts[2])))
-)
-
-# Compute percentages
-sizes_data$fraction = sizes_data$values / sum(sizes_data$values)
-
-# Compute the cumulative percentages (top of each rectangle)
-sizes_data$ymax = cumsum(sizes_data$fraction)
-
-# Compute the bottom of each rectangle
-sizes_data$ymin = c(0, head(sizes_data$ymax, n = -1))
-
-# Compute label position
-sizes_data$labelPosition <- (sizes_data$ymax + sizes_data$ymin) / 2
-
-# Compute a good label
-sizes_data$label <- paste0(sizes_data$category, "\n value: ", sizes_data$values)
-
-
-or_palette <- c("#b8641d", "#ebc5a4", "#d9a171", "#c98449")
-
-# Make the plot
-sizes <- ggplot(sizes_data, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = category)) +
-  geom_rect() +
-  geom_text(x = 3.5, aes(y = labelPosition, label = category), size = 4) +
-  scale_fill_manual(values = or_palette) +
-  coord_polar(theta = "y") +
-  xlim(c(0.8, 4)) +
-  theme_void() +
-  theme(legend.position = "none") +
-  
-  geom_image(data = data.frame(xx = 0.8, yy = 0, image = "data/images/vegetation_icon.png"), mapping = aes(xx, yy, image = image), size = .3, inherit.aes = FALSE)
-
-sizes
-
+size_plot <- plot_classes(plant_points,
+                          variable_name = "size",
+                          colour_palette = c("#b8641d", "#ebc5a4", "#d9a171", "#c98449"),
+                          image_path = "data/images/vegetation_icon.png")
 
 ### Endemism ###
-endemism_props <- table(plant_points$endemism)
-endemism_max_prop <- base::round((max(endemism_props) / sum(endemism_props)) * 100)
-endemism_min_prop <- base::round((min(endemism_props) / sum(endemism_props)) * 100)
-
-endemism_data <- data.frame(
-  category = 1:100,
-  values = c(rep(1, endemism_max_prop), rep(0, endemism_min_prop))
-)
-
-# Compute the cumulative percentages (top of each rectangle)
-endemism_data$ymax = 1:100
-
-# Compute the bottom of each rectangle
-endemism_data$ymin = c(0, head(endemism_data$ymax, n = -1))
-
-pr_palette <- c(rep("#a072a6", endemism_max_prop),  rep("#e8e8e8", endemism_min_prop))
-
-# Make the plot
-endemism <- ggplot(endemism_data, aes(ymax = ymax, ymin = ymin, xmax = 5, xmin = 1, fill = factor(category))) +
-  geom_rect(color = "white", size = 1) +
-  geom_point(aes(x = 0, y = -max(values) * 0.5), color = "white", size = 50) +
-  #geom_text(x = 3.5, aes(y = labelPosition, label = toupper(category)), size = 4) +
-  scale_fill_manual(values = pr_palette) +
-  coord_polar(theta = "y") +
-  xlim(c(0, 5)) +
-  theme_void() +
-  theme(legend.position = "none") +
-  
-  geom_text(data = data.frame(xx = 0, yy = -max(endemism_data$values) * 0.5, label = paste0(endemism_max_prop, "%\nNATIVE")), mapping = aes(xx, yy, label = label), size = 4, inherit.aes = FALSE)
-
-endemism
-
+endemism_plot <- plot_percent(plant_points,
+                              variable_name = "endemism",
+                              colour = "#a072a6",
+                              label = "NATIVE")
 
 ### Type ###
-type_props <- table(plant_points$type)
-type_max_prop <- base::round((max(type_props) / sum(type_props)) * 100)
-type_min_prop <- base::round((min(type_props) / sum(type_props)) * 100)
-
-type_data <- data.frame(
-  category = 1:100,
-  values = c(rep(1, type_max_prop), rep(0, type_min_prop))
-)
-
-# Compute the cumulative percentages (top of each rectangle)
-type_data$ymax = 1:100
-
-# Compute the bottom of each rectangle
-type_data$ymin = c(0, head(type_data$ymax, n = -1))
-
-br_palette <- c(rep("#a19a65", type_max_prop),  rep("#e8e8e8", type_min_prop))
-
-# Make the plot
-type <- ggplot(type_data, aes(ymax = ymax, ymin = ymin, xmax = 5, xmin = 1, fill = factor(category))) +
-  geom_rect(color = "white", size = 1) +
-  geom_point(aes(x = 0, y = -max(values) * 0.5), color = "white", size = 50) +
-  #geom_text(x = 3.5, aes(y = labelPosition, label = toupper(category)), size = 4) +
-  scale_fill_manual(values = br_palette) +
-  coord_polar(theta = "y") +
-  xlim(c(0, 5)) +
-  theme_void() +
-  theme(legend.position = "none") +
-  
-  geom_text(data = data.frame(xx = 0, yy = -max(type_data$values) * 0.5, label = paste0(type_max_prop, "%\nEVERGREEN")), mapping = aes(xx, yy, label = label), size = 4, inherit.aes = FALSE)
-
-type
-
+type_plot <- plot_percent(plant_points,
+                          variable_name = "type",
+                          colour = "#a19a65",
+                          label = "EVERGREEN")
 
 ### Species richness ###
 richness_data <- data.frame(
-  id = seq(1, length(unique(plant_points$species)), 1),
-  value = sapply(unique(plant_points$species), function(sp) length(which(plant_points$species == sp)))
+  id = seq(1, length(unique(spatial_points$species)), 1),
+  value = sapply(unique(spatial_points$species), function(sp) length(which(spatial_points$species == sp)))
 )
-
-# label_data <- data
-# 
-# # calculate the ANGLE of the labels
-# number_of_bar <- nrow(label_data)
-# angle <-  90 - 360 * (label_data$id - 0.5) / number_of_bar     # I subtract 0.5 because the letter must have the angle of the center of the bars. Not extreme right(1) or extreme left (0)
-# 
-# # calculate the alignment of labels: right or left
-# # If I am on the left part of the plot, my labels have currently an angle < -90
-# label_data$hjust <- ifelse(angle < -90, 1, 0)
-# 
-# # flip angle BY to make them readable
-# label_data$angle <- ifelse(angle < -90, angle + 180, angle)
 
 # Make the plot
 richness <- ggplot(richness_data, aes(x = as.factor(id), y = value)) +       # Note that id is a factor. If x is numeric, there is some space between the first bar
@@ -308,24 +103,11 @@ n_months <- length(months)
 
 phenology_data <- data.frame(
   id = seq_len(n_months),
-  value = sapply(months, function(month) sum(grepl(month, plant_points$phenology)))
+  value = sapply(months, function(month) sum(grepl(month, spatial_points$phenology)))
 )
 
 img <- readPNG("data/images/flower_icon.png")
 g <- rasterGrob(img, interpolate = TRUE)
-
-# label_data <- data
-# 
-# # calculate the ANGLE of the labels
-# number_of_bar <- nrow(label_data)
-# angle <-  75 - 360 * (label_data$id - 0.5) / number_of_bar     # I subtract 0.5 because the letter must have the angle of the center of the bars. Not extreme right(1) or extreme left (0)
-# 
-# # calculate the alignment of labels: right or left
-# # If I am on the left part of the plot, my labels have currently an angle < -90
-# label_data$hjust <- ifelse(angle < -90, 1, 0)
-# 
-# # flip angle BY to make them readable
-# label_data$angle <- ifelse(angle < -90, angle + 180, angle)
 
 # Make the plot
 phenology <- ggplot(phenology_data, aes(x = as.factor(id), y = value)) +       # Note that id is a factor. If x is numeric, there is some space between the first bar
@@ -361,7 +143,7 @@ phenology
 ### Coverage at 1m ###
 coverage_data <- data.frame(
   id = factor(seq_len(n_year_groups)),
-  value = extract_1m_coverage(years_1_30)
+  value = extract_1m_coverage(spatial_list)
 )
 
 # Make the plot
@@ -396,7 +178,7 @@ coverage
 
 
 ### Connectivity ###
-connectivity_data_w <- estimate_connectivity(years_1_30)
+connectivity_data_w <- estimate_connectivity(spatial_list)
 
 connectivity_score <- base::round(mean(as.matrix(connectivity_data_w)), 2)
 
